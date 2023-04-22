a88103ab8a0789e0ee50d1956970e714a75dab6f
Jide-codes <hezekiaholajide3@gmail.com>
2023-04-21 22:29:35 -1100

    Added whitenoise

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -39,11 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'base'
+    'base',
+    'whitenoise.runserver_nostatic',  
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
